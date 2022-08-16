e9d74c4e5102c7ab42441c53a77825e491fcdcae
Abhishek <dhananjaykolaskar2001@gmail.com>
2022-08-16 12:58:20 +0530

    mvt12

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ function App() {
           name ?
             <>
               <ScrollToBottom initialScrollBehavior='smooth'>
-                <Box component='div' sx={{ width: 580, height:{xl:715,xs:'800px'} }} >
+                <Box component='div' sx={{ width: 580, height:{xl:715,xs:'600px'} }} >
 
                   {chats?.map((c, i) => (
 
